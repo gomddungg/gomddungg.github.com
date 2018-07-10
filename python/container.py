94a2cf58b2ec71bdfdffea85fb3708ad45982f1e
gomddungg <gomddungg@naver.com>
2018-07-10 19:09:06 +0900

    container

---
+++ python/container.py
@@ -0,0 +1,17 @@
+'''
+컨테이너?
+
+컨테이너 = 무언가 여러가지를 담는 큰 그릇
+
+in Python, 리스트!
+
+'''
+
+print(type('gomddungg')) #type = 데이터 형식을 불러와라
+
+name = 'gomddungg'
+print(name)
+
+names = ['gomddungg','you','me'] #컨테이너
+print(type(names))
+print(names)
